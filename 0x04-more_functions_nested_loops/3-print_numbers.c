8f1f5cf62f41a2771b28b1cef7e57c07359cdc9f
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-21 12:31:54 +0000

    task 3 | print number 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * print_numbers - prints numbers 0-9
+  * Return: always 0
+  */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+}
