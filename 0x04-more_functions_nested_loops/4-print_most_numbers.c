ce5be29890045e90c5da9fcfff3e3f3ee803f98b
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-21 13:24:39 +0000

    task 4 | loop 1-9 jump: 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * print_most_numbers - prints 0-9 minus 2 & 4
+  * Return: always 0
+  */
+
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		if (n != 2 && n != 4)
+		{
+			_putchar(a + '0');
+		}
+		n++
+	}
+	_putchar('\n');
+}
