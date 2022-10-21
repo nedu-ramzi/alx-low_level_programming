ab67e4b928c97c81afb37c704f474e234564ba71
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-21 17:51:17 +0000

    task 6 | print straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * print_line -mprints a straight line using '_'
+  * @n: the number of '_' to be printed
+  */
+
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
