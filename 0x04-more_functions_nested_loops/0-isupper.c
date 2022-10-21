f6bbed483f502e39ae88a3192f903732e8850ffa
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-21 11:07:47 +0000

    task 0 | isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
-  * main - Check for isupper prototype
   * _isupper - checks if varaiable c is uppercase
   * @c: Accept character value
   * Return: return 0
@@ -8,7 +7,7 @@
 
 int _isupper(int c)
 {
-	if (isupper(c))
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
