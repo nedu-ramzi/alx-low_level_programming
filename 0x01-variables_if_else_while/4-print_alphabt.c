4fc58b5f45e1f407200243093e374cb2d46aaa39
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-22 10:12:50 +0000

    task 4 | lowercase alphabet without q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - print all lowercase alphabet that is not q and e
+  * Return: Always 0
+  */
+int main(void){
+	char lower = 'a';
+
+	while (lower <= 'z')
+	{
+		if (lower == 'q' && lower == 'e')
+		{
+			putchar(lower);
+		}
+		lower ++;
+	}
+	return (0);
+}
