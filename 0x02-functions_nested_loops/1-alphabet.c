8a664fbd6a17ab1ee44042d21f2215cf4116bc67
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-22 16:15:37 +0000

    task 1 | lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_alphabet - prints the program
+  * Return: 0
+  */
+void print_alphabet(void)
+{
+	char al = 'a';
+
+	while (al <= 'z')
+	{
+		_putchar(al);
+	}
+	_putchar('\n');
+	return (0);
+}
