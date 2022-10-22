fadffdfff43f6b1c009618d294eb25a2f6ba777f
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-22 12:18:07 +0000

    task 9 | print 1-10 with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - prints all possible combinations of single-digit numbers
+  * Return: 0
+  */
+int main(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{	
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
