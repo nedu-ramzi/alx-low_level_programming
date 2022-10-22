33456aa80f5b5c0754ef07d970f844e3f0bb3a7a
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-22 11:27:15 +0000

    Task 6 | using putchar to print an int type variable

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,12 +6,13 @@
 int main(void)
 {
 	int i = 0;
-	 while (i < 10)
-	 {
+
+	while (i < 10)
+	{
 		putchar(i + '0');
 		i++;
-	 }
-	 putchar('\n');
-	 return (0);
+	}
+	putchar('\n');
+	return (0);
 }
 
