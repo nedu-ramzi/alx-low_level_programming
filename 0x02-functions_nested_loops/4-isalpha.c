88a66c8a19ea2afde40e1b20cdbcd80625214f12
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-23 21:05:39 +0000

    task 4 | checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  * _isalpha - checks for alphabetic characters
+  * @c: hold the value
+  * Return:0
+  */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
