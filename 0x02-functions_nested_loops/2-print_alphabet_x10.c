31696137e45cbd657b0de3569b5f3653ecfc5a0d
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-23 16:05:34 +0000

    task 2 | lowercase alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * main - print ten times the alphabet, in lowercase
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		int az = 'a';
+
+		while (az <= 'z')
+		{
+			_putchar(az + '0');
+			az++;
+		}
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
+
