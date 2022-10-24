fca65e711938edb638d05766612aec22b4110bd8
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-24 06:29:51 +0000

    task 5 | sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,25 @@
 /**
   * print_sign - prints the sign of a number
   * @n: holds the value n
-  * Return: 0
+  * Return: 0, -1 and 1
   */
 
 int print_sign(int n)
 {
 	if (n > 0)
-	 {
-		 return (1);
+	{
 		_putchar(+);
-	 }
-	 else if (n == 0)
-	 {
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
 		return (0);
-		_putchar(0)
-	 }
-	 else
-	 {
+	}
+	else
+	{
+		_putchar('-1');
 		return (-1);
-		_putchar(-);
-	 }
+
+	}
 }
