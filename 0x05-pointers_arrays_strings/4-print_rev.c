ac38c3a8f0b9e0251ffc14bae2eafe26145f5a07
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-25 13:31:30 +0000

    task 4 | reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,15 @@
   */
 void print_rev(char *s)
 {
-	while (*s = '\0')
+	int i = 0;
+
+	while (s[i] = '\0')
 	{
-		*s--;
+		i++;
+	}
+	for (i = i -1; i >= 0; i++)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
