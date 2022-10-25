fedbe3a7f0ca15d1972c78525f6ccb7b8d6c5852
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-25 13:27:14 +0000

    task 4 | reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse
+  * @s: variable for string value
+  */
+void print_rev(char *s)
+{
+	while (*s = '\0')
+	{
+		*ss--;
+	}
+	_putchar('\n');
+}
