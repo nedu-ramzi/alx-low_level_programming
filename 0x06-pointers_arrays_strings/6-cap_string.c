e501c62bcfc9df8ffd8d5dc199d82b509c76e2c0
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-27 22:54:12 +0000

    task 6 | cap string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+  * cap_string - captalizes all words of a sting
+  * @str: string
+  * Return: pointer to string
+  */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+		{
+			i++;
+		}
+		if (str[i - 1] == ' ' || str[i - 1] == '\t'
+			str[i - 1] == '\n' || str[i - 1] == ','
+			str[i - 1] == ';' || str[i - 1] == '.'
+			str[i - 1] == '!' || str[i - 1] == '?'
+			str[i - 1] == '"' || str[i - 1] == '('
+			str[i - 1] == ')' || str[i - 1] == '{'
+			str[i - 1] == '}' || i == 0)
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
