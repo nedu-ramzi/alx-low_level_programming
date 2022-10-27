b95fa65adef80d5548cc26976f1df8701f2f5057
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-27 15:00:19 +0000

    task 0 | string concatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * *_strcat - concatenates two strings
+  * @*dest: holds dest
+  * @*src: holds src
+  * Return: 0
+  */
+
+char *_strcat(char *dest, char *src)
+{
+	if (*dest != '\0')
+	{
+		*dest =+ *src;
+	}
+}
+
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers, arrays and strings version 2
