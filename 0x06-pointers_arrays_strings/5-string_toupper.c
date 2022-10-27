450cad4511c4346838d114ec539389b331213e22
nedu-ramzi <ramseychinedu@gmail.com>
2022-10-27 22:31:21 +0000

    task 5 | change lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * string_toupper - changes all lower letters of a string to upper
+  * @n: pointer
+  * Return: char
+  */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+		{
+			n[i] = n[i] - 32;
+			i++;
+		}
+	}
+	return (n);
+}
