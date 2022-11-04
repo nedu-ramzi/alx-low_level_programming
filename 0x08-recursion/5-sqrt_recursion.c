e01ee7e0d34f3036ffd7da7a97ddadddd315ab1d
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-04 10:44:55 +0000

    Task 5 | Natural Square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,9 @@
 #include "main.h"
-
+int actual_sqrt_recursion(int n, int i);
 /**
   * _sqrt_recursion - returns the natural square root of a number
   * @n: value
+  * Return: The square root
   */
 
 int _sqrt_recursion(int n)
@@ -11,5 +12,5 @@ int _sqrt_recursion(int n)
 	{
 		return(-1);
 	}
-	return (_sqrt_recursion(n, 0));
+	return (actual_sqrt_recursion(n, 0));
 }
