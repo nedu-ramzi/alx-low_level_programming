d5aaf13b65644b762b4a1d7d3ad2c509623ca472
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-04 10:51:11 +0000

    Task 5 | Natural Square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,3 +14,23 @@ int _sqrt_recursion(int n)
 	}
 	return (actual_sqrt_recursion(n, 0));
 }
+/**
+  * actual_sqrt_recursion - recurses to finf the natural
+  * square root of a number
+  * @n: number to calculate the square root of
+  * @i: iterator
+  *
+  * Return: the resulting squre root
+  */
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else if (i * i == n)
+	{
+		return (i);
+	}
+	return (actual_sqrt_recursion(n, i + 1));
+}
