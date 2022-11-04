6d2607f428028512858f8ab0b8649364c0f4f51b
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-04 07:52:59 +0000

    task 1 | Reversed string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  * _print_rev_recursion - prints a string in reverse
+  * @s: Hold the string
+  */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_print_rev_recursion(s--);
+	}
+	else
+	{
+		return (0);
+	}
+}
