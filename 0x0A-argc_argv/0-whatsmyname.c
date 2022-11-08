99e0d80bdceb62a19174dbb89ba4a288f06e1a79
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-08 22:39:10 +0000

    Task 0 | programme name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
-  * whatsmyname - prints it name
+  * main - prints the name of the program
+  * @argc: number of argument
+  * @argv: array of character argument
+  * Return: 0
   */
 
 
 int whatsmyname(int argc, char *argv[])
 {
-	printf("\n",argv[0]);
+	printf("\n",argv);
 }
