8b50596e0ac4d3498a5215c5be435356ef5dbd18
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-09 06:49:15 +0000

    task 3 | conversts string to int and multiplies the numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,55 +1,50 @@
-#include <stdio.h>
-#include "main.c"
+nclude <stdio.h>
+#include "main.h"
+
 
 /**
-  * _atoi - converts a string to an integr
+  * _atoi - converts a string to an integer
   * @s: string to be converted
   * Return: the int converted from the string
   */
-
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit = 0;
-
+	int i, d, n, len, f, digit;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 	while (s[len] != '\0')
-	{
-		len++;
-	}
+	len++;
 	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
 		{
+			if (s[i] == '-')
 			++d;
-		}
-		if (s[i] >= '0' && s[i] <= '9')
+			if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-			{
-				digit = -digit;
+																							digit = s[i] - '0';
+																							if (d % 2)
+																							digit = -digit;
+																							n = n * 10 + digit;
+																							f = 1;
+																							if (s[i + 1] < '0' || s[i + 1] > '9')
+																							break;
+																							f = 0;
+																							}
+			i++;
 			}
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-			{
-				break;
-			}
-			f = 0;
-		}
-		i++;
-	}
-	if (f == 0)
-	{
-		return (0);
-	}
-	return (n);
+			if (f == 0)
+			return (0);
+			return (n);
 }
 
 /**
-  * main - multiplies two numbers
-  * @argc: number of argument
-  * @argv: array of argument
-  * Return: 0 (success), 1 (Error)
+  *  main - multiplies two numbers
+  *   @argc: number of arguments
+  *    @argv: array of arguments
+  *    Return: 0 (Success), 1 (Error)
   */
 int main(int argc, char *argv[])
 {
@@ -60,12 +55,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
-
 	printf("%d\n", result);
-
 	return (0);
 }
