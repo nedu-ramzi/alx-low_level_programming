4f35cf379da73e0af03fba643028b79e697dfb4f
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-09 06:11:57 +0000

    task 2 | prints all argument passed

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - prints all argument it receives
+  * @argc: number of argument
+  * @argv: character of argument
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argv[i]);
+	}
+	return (0);
+}
