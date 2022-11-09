16830ba28e8a5658475efb5721999bc8d109f92f
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-09 09:27:55 +0000

    task 3 | conversts string to int and multiplies the numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,4 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 
@@ -7,37 +7,42 @@ nclude <stdio.h>
   * @s: string to be converted
   * Return: the int converted from the string
   */
+
 int _atoi(char *s)
 {
 	int i, d, n, len, f, digit;
+
 	i = 0;
 	d = 0;
 	n = 0;
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
-	len++;
+		len++;
+
 	while (i < len && f == 0)
-		{
-			if (s[i] == '-')
+	{
+		if (s[i] == '-')
 			++d;
-			if (s[i] >= '0' && s[i] <= '9')
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-																							digit = s[i] - '0';
-																							if (d % 2)
-																							digit = -digit;
-																							n = n * 10 + digit;
-																							f = 1;
-																							if (s[i + 1] < '0' || s[i + 1] > '9')
-																							break;
-																							f = 0;
-																							}
-			i++;
-			}
-			if (f == 0)
-			return (0);
-			return (n);
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
 }
 
 /**
@@ -46,6 +51,7 @@ int _atoi(char *s)
   *    @argv: array of arguments
   *    Return: 0 (Success), 1 (Error)
   */
+
 int main(int argc, char *argv[])
 {
 	int result, num1, num2;
@@ -58,6 +64,8 @@ int main(int argc, char *argv[])
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
