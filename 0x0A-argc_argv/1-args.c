b2c80c18df661321c11362dc4b17e127e502b715
nedu-ramzi <ramseychinedu@gmail.com>
2022-11-09 05:56:44 +0000

    task 1 | number of argument passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  * main - prints the number of argument passed into it
+  * @argc: number of argument
+  * @argv: character of argument
+  * Return: 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
