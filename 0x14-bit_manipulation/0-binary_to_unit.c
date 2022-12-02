8020c90bb2df89ac3e62aec2b25640261dd1922f
nedu-ramzi <ramseychinedu@gmail.com>
2022-12-02 21:02:30 +0000

    task 0 | binary to unit

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -7,31 +7,23 @@
   *
   * Return: unsigned int.
   */
-
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int val;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	i = 0;
+	val = 0;
+	while (b[i])
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
-			return (0);
-		}
+		if (b[i] < '0' || b[i] > '1')
+													return (0);
+													val = 2 * val + (b[i] - '0');
+													i++;
+											}
 
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
-	}
-	return (ui);
+	return (val);
 }
